512f34ea040bbb6d9322bde186db985c1b0df2d4
BenjaminTan <yiren.tan@gmail.com>
2019-07-15 11:09:15 -0700

    Finish API Util

---
+++ frontend/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// import configureStore from './store/store';
+
+// testing
+import { getDrinks, getDrinkDetails, getIngredientPics, getIngredients, getDrinksByIngredient } from './util/drinks_api_util';
+
+
+
+  // const store = configureStore();
+
+  //testing
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  window.getDrinks = getDrinks;
+  window.getDrinkDetails = getDrinkDetails;
+  window.getIngredientPics = getIngredientPics;
+  window.getIngredients = getIngredients;
+  window.getDrinksByIngredient = getDrinksByIngredient;
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
