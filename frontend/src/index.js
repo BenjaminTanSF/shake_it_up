46073764755acc48c73ba68e81df73672cf792dc
BenjaminTan <yiren.tan@gmail.com>
2019-07-15 21:06:23 -0700

    Add testing to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//testing
+import configureStore from './store/store';
+import { fetchAllDrinks, fetchSingleDrink } from './actions/drinks_actions';
+
+const store = configureStore();
+
+//testing
+window.getState = store.getState;
+window.dispatch = store.dispatch;
+window.fetchAllDrinks = fetchAllDrinks;
+window.fetchSingleDrink = fetchSingleDrink;
+
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
