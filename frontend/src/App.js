1c39afa40355a3b2889aa2dd3a56ff02796a041f
SkiesXR <pkrasnick@gmail.com>
2019-07-16 15:23:56 -0700

    Build header and menu components, add to App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import BYOC from './components/byoc/byoc';
+import Header from './components/header';
+import Menu from './components/menu';
 import Discover from './components/discover/discover';
 import DrinksIndexContainer from './components/drinks/drinks_index_container';
 // import { DrinksShowContainer } from './components/drinks/drinks_show_container';
@@ -11,6 +14,7 @@ import IngredientShowContainer from './components/ingredients/ingredient_show_co
 function App() {
   return (
     <div className="shake-it-up-container">
+      <Header />
       {/* <Link to="/"> */}
       {/* <h1>Shake It Up</h1> */}
       {/* </Link> */}
@@ -21,10 +25,11 @@ function App() {
         <Route exact path="/ingredients" component={IngredientsIndexContainer} />
         <Route exact path="/ingredients/:ingredient_name" component={IngredientShowContainer} />
         <Route exact path="/drinks" component={DrinksIndexContainer} />
+        <Route exact path="/byoc" component={ BYOC } />
         <Route path="/" component={Discover} />
         {/* <Route exact path="/drinks/:drink_id" component={DrinksShowContainer}/> */}
       </Switch>
-
+    < Menu / >
     </div>
   );
 }
---
+++ frontend/src/components/byoc/byoc.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import byocStyles from '../../styles/discover/byoc.scss'
+
+class BYOCPrompt extends React.Component {
+
+    render() {
+        return (
+           <div className="byoc-container">
+               <h1>Pick your base spirit</h1>
+           </div>
+        );
+    }
+
+}
+
+export default BYOCPrompt;
+
