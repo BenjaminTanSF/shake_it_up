529b13160d794d42ba9ba8846b000f116578848e
frankie siino <frankie.siino@gmail.com>
2019-07-15 19:48:45 -0700

    Add store to Root file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 // ReactDOM.render(<App />, document.getElementById('root'));
 const store = configureStore();
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
