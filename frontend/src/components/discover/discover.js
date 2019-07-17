d25253e96578b10fa4850b05aafa790bfa38c06a
BenjaminTan <yiren.tan@gmail.com>
2019-07-17 09:44:47 -0700

    Add slide feature on discover page

--- frontend/src/components/discover/discover.js
+++ frontend/src/components/discover/discover.js
@@ -7,43 +7,47 @@ class Discover extends React.Component {
   render() {
     return (
       <div className="discover-page-container">
-          <div className="discover-hero">
-            <div id="hero-tag">
-              Let's build a cocktail, together!
+        <div className="discover-hero">
+          <div id="hero-tag">
+            Let's build a cocktail, together!
             </div>
-            <Link id="get-started" to="/byoc">Get Started</Link>            
-          </div>
-          <div className="discover-featured">
-            <h1>Featured Cocktails</h1>
-          </div>
+          <Link id="get-started" to="/byoc">Get Started</Link>
+        </div>
+        <div className="discover-featured">
+          <h1>Featured Cocktails</h1>
+        </div>
+        <div className="discover-slideshow-wrapper">
+
           <div className="discover-slideshow">
-              <div className="discover-slideshow-item">
-                <Link to="/drinks/11003">Negroni</Link>
-              </div>
-              <div className = "discover-slideshow-item" >
-                <Link to="/drinks/17836">Acapulco</Link> 
-              </div>              
-              <div className = "discover-slideshow-item" >
-                <Link to="/drinks/11243">Chocolate Black Russian</Link> 
-              </div>              
-              <div className = "discover-slideshow-item" >
-                <Link to="/drinks/11375">Foxy Lady</Link> 
-              </div>              
+            <div className="discover-slideshow-item">
+              <Link to="/drinks/11003">Negroni</Link>
             </div>
-          <div className="discover-base">
-            <h1>Base Spirits</h1>
-            <div className="discover-base-spiritlist">
-                {/* <Link id="d-bs-whiskey" className="discover-spirit" to="">Whiskey</Link> */}
-                <div className="bs-overlay">
-                  <Link id="d-bs-whiskey" className="discover-spirit" to="">Whiskey</Link>
-                </div>
-                <Link id="d-bs-vodka" className="discover-spirit" to="">Rum</Link>
-                <Link id="d-bs-tequila" className="discover-spirit" to="">Vodka</Link>
-                <Link id="d-bs-rum" className="discover-spirit" to="">Tequila</Link>
-                <Link id="d-bs-gin" className="discover-spirit" to="">Gin</Link>
-                <Link id="d-bs-brandy" className="discover-spirit" to="">Brandy</Link>
+            <div className="discover-slideshow-item" >
+              <Link to="/drinks/17836">Acapulco</Link>
+            </div>
+            <div className="discover-slideshow-item" >
+              <Link to="/drinks/11243">Chocolate Black Russian</Link>
+            </div>
+            <div className="discover-slideshow-item" >
+              <Link to="/drinks/11375">Foxy Lady</Link>
             </div>
           </div>
+
+        </div>
+        <div className="discover-base">
+          <h1>Base Spirits</h1>
+          <div className="discover-base-spiritlist">
+            {/* <Link id="d-bs-whiskey" className="discover-spirit" to="">Whiskey</Link> */}
+            <div className="bs-overlay">
+              <Link id="d-bs-whiskey" className="discover-spirit" to="">Whiskey</Link>
+            </div>
+            <Link id="d-bs-vodka" className="discover-spirit" to="">Rum</Link>
+            <Link id="d-bs-tequila" className="discover-spirit" to="">Vodka</Link>
+            <Link id="d-bs-rum" className="discover-spirit" to="">Tequila</Link>
+            <Link id="d-bs-gin" className="discover-spirit" to="">Gin</Link>
+            <Link id="d-bs-brandy" className="discover-spirit" to="">Brandy</Link>
+          </div>
+        </div>
       </div>
     );
   }
