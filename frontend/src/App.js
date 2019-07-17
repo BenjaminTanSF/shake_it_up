44db02a0b3ce88b5960b7d9e61784e1bdbc27597
BenjaminTan <yiren.tan@gmail.com>
2019-07-16 18:40:48 -0700

    Complete initial state of BYOC result

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import BYOC from './components/byoc/byoc';
+import BYOCContainer from './components/byoc/byoc_container';
+import BYOCResults from './components/byoc/byoc-results';
 import Header from './components/header';
 import Menu from './components/menu';
 import Discover from './components/discover/discover';
@@ -25,7 +26,8 @@ function App() {
         <Route exact path="/ingredients" component={IngredientIndexContainer} />
         <Route exact path="/ingredients/:ingredient_name" component={IngredientShow} />
         <Route exact path="/drinks" component={DrinkIndexContainer} />
-        <Route exact path="/byoc" component={BYOC} />
+        <Route exact path="/byoc" component={BYOCContainer} />
+        <Route exact path="/byoc/:spirit_name" component={BYOCResults} />
         <Route path="/" component={Discover} />
         {/* <Route exact path="/drinks/:drink_id" component={DrinkShowContainer}/> */}
       </Switch>
--- frontend/src/components/byoc/byoc-results.jsx
+++ frontend/src/components/byoc/byoc-results.jsx
@@ -6,9 +6,46 @@ class BYOCResults extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            ingredients: this.props.location.state.ingredients,
+            images: this.props.location.state.images,
+            compatibleIngredients: null,
+            drinks: null
+        }
+    }
+
+    componentDidMount() {
+        let drinks = [];
+        let compatibleIngredients = [];
+        this.state.ingredients.forEach(ing => {
+            let ingDrinks = ing.drinks;
+            ingDrinks.forEach(drink => {
+                if (!drinks.map(d => d.strDrink).includes(drink.strDrink)) {
+                    drinks.push(drink);
+                }
+            })
+        });
+
+        drinks.forEach(drink => {
+            for (let i = 1; i <= 15; i++) {
+                if (drink[`strIngredient${i}`] === "" || drink[`strIngredient${i}`] === undefined || drink[`strIngredient${i}`] === null) {
+                    break;
+                } else if (!compatibleIngredients.map(ci => ci.name).includes(drink[`strIngredient${i}`].toLowerCase())) {
+                    compatibleIngredients.push(
+                        this.state.images.find(imageObj => imageObj.name === drink[`strIngredient${i}`].toLowerCase())
+                    );
+                }
+            }
+        })
+
+        this.setState({
+            drinks,
+            compatibleIngredients
+        });
     }
 
     render() {
+        console.log(this.state);
         return (
             <div className="byoc-results-container">
 
