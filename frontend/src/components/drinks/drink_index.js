a474c1ccd753f8f7111cd1cda05d4cc3e4decf30
SkiesXR <pkrasnick@gmail.com>
2019-07-17 11:30:00 -0700

    Flex grid index

--- frontend/src/components/drinks/drink_index.js
+++ frontend/src/components/drinks/drink_index.js
@@ -51,20 +51,23 @@ class DrinksIndex extends React.Component {
             {/* <span id = "emoji"> 🔍🔎 </span> */}
           </label>
         </div>
-        <h1>Drinks</h1>
+        <div id="drink-index-h1-container">
+          <h1>Cocktails</h1>
+        </div>
+        
 
+        <div className="drink-tile-container">
         {Object.values(this.state.shownDrinks).map(drink =>
           <div className="drink-index-tile" key={drink.idDrink}>
             <Link to={`/drinks/${drink.idDrink}`}>
               <img alt={drink.strDrink} className="drink-idx-img" src={drink.strDrinkThumb} width="70%" />
-              <br />
-              <div className="drink-index-name">
-                {drink.strDrink}
-              </div>
+              <span className="drink-index-name">{drink.strDrink}</span>
             </Link>
+            <hr></hr>
           </div>
-
         )}
+        
+        </div>
       </div>
     )
   }
@@ -72,3 +75,7 @@ class DrinksIndex extends React.Component {
 
 }
 export default DrinksIndex;
+
+// TODO: Styling for Drinks Index results
+// TODO: Styling / Interaction Design for Search Bar
+// TODO: Add drink ingredients below drink title
