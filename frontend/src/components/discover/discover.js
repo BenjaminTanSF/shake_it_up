3e4b4679a93d4ac303b13f2ae735e2d308c62270
frankie siino <frankie.siino@gmail.com>
2019-08-05 13:30:09 -0700

    Further format carousel and add text shadowing to base spirits

--- frontend/src/components/discover/discover.js
+++ frontend/src/components/discover/discover.js
@@ -22,54 +22,56 @@ class Discover extends React.Component {
             <div className="discover-slideshow-item">
               <Link to="/drinks/11003">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg" alt="Negroni" />
-                Negroni
+              <div className="discover-slideshow-title">Negroni</div>
               </Link>
             </div>
             <div className="discover-slideshow-item" >
               <Link to="/drinks/17836">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/vtpsvr1472811976.jpg" alt="Acapulco" />
-                Acapulco
+                <div className="discover-slideshow-title">Acapulco</div>
               </Link>
             </div>
             <div className="discover-slideshow-item" >
               <Link to="/drinks/11243">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/yyvywx1472720879.jpg" alt="Chocolate Black Russian"/>
-                Chocolate Black Russian
-                </Link>
+                  <div className="discover-slideshow-title">Chocolate Black Russian</div>
+              </Link>
             </div>
             <div className="discover-slideshow-item" >
               <Link to="/drinks/11375">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/r9cz3q1504519844.jpg" alt="Foxy Lady"/>
-                Foxy Lady
+                <div className="discover-slideshow-title">Foxy Lady</div>
               </Link>
             </div>
             <div className="discover-slideshow-item" >
               <Link to="/drinks/11009">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/3pylqc1504370988.jpg" alt="Moscow Mule"/>
-                Moscow Mule
+                <div className="discover-slideshow-title">Moscow Mule</div>
               </Link>
             </div>
             <div className="discover-slideshow-item" >
               <Link to="/drinks/14229">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/xxsxqy1472668106.jpg" alt="747"/>
-                747
+                <div className="discover-slideshow-title">747</div>
               </Link>
             </div>
             <div className="discover-slideshow-item" >
               <Link to="/drinks/15300">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg" alt="3-Mile Long Island Iced Tea" />
-                3-Mile Long Island Iced Tea
+                <div className="discover-slideshow-title">3-Mile Long Island Iced Tea</div>
               </Link>
             </div>
             <div className="discover-slideshow-item" >
-              <Link to="/drinks/17079">
-                <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/rvyvxs1473482359.jpg" alt="Baby Guinness" />
-                Baby Guinness</Link>
+              <Link to="/drinks/14538">
+                <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/uwqpvv1461866378.jpg" alt="Bumble Bee #1" />
+                <div className="discover-slideshow-title">Bumble Bee #1</div>
+              </Link>
             </div>
             <div className="discover-slideshow-item" >
               <Link to="/drinks/11064">
                 <img className="discover-slideshow-img" src="https://www.thecocktaildb.com/images/media/drink/k1xatq1504389300.jpg" alt="Banana Daiquiri" />
-                Banana Daiquiri</Link>
+                <div className="discover-slideshow-title">Banana Daiquiri</div>
+              </Link>
             </div>
           </div>
 
