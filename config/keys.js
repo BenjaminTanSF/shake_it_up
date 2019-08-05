e1e4a507980ef089cb1bb011a792bdd15f55f3b5
robmroy <robmyroy@gmail.com>
2019-08-05 13:43:22 -0700

    fixed keys to retry merge

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,5 @@
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = require('./keys_prod');
-// } else {
-//   module.exports = require('./keys_dev');
-// }
-module.exports = {
-  mongoURI: 'mongodb+srv://rob:SG1MoUXjjLISS0bs@cluster0-rc0zr.mongodb.net/test?retryWrites=true&w=majority'
-            //Make sure this is your own unique string
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
