53448fd66c5688899b938ce050cad82ee431004e
frankie siino <frankie.siino@gmail.com>
2019-08-06 13:43:45 -0700

    Add tile and styling to ingred show page

--- frontend/src/components/ingredients/ingredient_show.js
+++ frontend/src/components/ingredients/ingredient_show.js
@@ -42,19 +42,30 @@ class  IngredientShow extends React.Component  {
     <div className="ingred-show-container">
       <h1>{ingredient.name}</h1>
       <img src={process.env.PUBLIC_URL + `/images/${fileName}`} alt={ingredient.name} />
-      {ingredient.description ? <p>{ingredient.description}</p> : null}
+      
+      <div className="is-tile-container">
+        <h2>Description</h2>
+        {ingredient.description ? <p>{ingredient.description}</p> : null}
+      </div>
+
       <br />
-      <div className="ingred-show-desc">Drinks this can make...</div>
-      {ingredient.drinks.map(drink => (
-        <div key={drink._id}>
-          <Link to={{
-            pathname: `/drinks/${drink.idDrink}`,
-            state: { drink: drink }
-          }}
-          >{drink.strDrink}</Link>
-          <br />
+      <div className="is-tile-container">
+        <div className="ingred-show-desc">
+          <h2>Drinks this can make...</h2>
+          <hr/>
         </div>
-      ))}
+        {ingredient.drinks.map(drink => (
+          <div className="is-drink-item" key={drink._id}>
+            <Link to={{
+              pathname: `/drinks/${drink.idDrink}`,
+              state: { drink: drink }
+            }}
+            >{drink.strDrink}
+            </Link>
+            <hr/>
+          </div>
+        ))}
+      </div>
     </div>
   )
         }
