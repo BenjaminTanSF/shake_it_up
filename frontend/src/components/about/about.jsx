94089bf32cce9db06e4248a8d5ee50b9a1e388ac
SkiesXR <pkrasnick@gmail.com>
2019-08-06 13:53:53 -0700

    About: Component added

---
+++ frontend/src/components/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class About extends React.Component {
+
+    render () {
+
+        return(
+            <div>
+                <span>About Page</span>
+            </div>
+        ); 
+    }
+}
+
+export default About;
