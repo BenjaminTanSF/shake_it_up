b69631bab0a3bdec6217bff1efec53c4c7739d84
SkiesXR <pkrasnick@gmail.com>
2019-08-06 15:53:32 -0700

    Discover: Adjust slideshow size. BYOC: Adjust size of base spirit cards so they are all visible on the page without scrolling

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -18,12 +18,19 @@ class About extends React.Component {
                             <img className={ `about-body-icon` } src={ GitHub } alt="GitHub"/>
                         </a>
                     </div>
-                    <div className="about-body-teammates">
+                    <div className="about-body-content">
                         <span>Team</span>
-                        <li>Benjamin Tan</li>
-                        <li>Frankie Siino</li>
-                        <li>Phillip Krasnick</li>
-                        <li>Rob Roy</li>
+                        <a href="https://benjamintan.dev/" target="_blank"><li>Benjamin Tan</li></a>
+                        <a href="https://www.frankiesiino.com/" target="_blank"><li>Frankie Siino</li></a>
+                        <a href="https://www.phillipkrasnick.com/" target="_blank"><li>Phillip Krasnick</li></a>
+                        <a href="https://robmroy.github.io/" target="_blank"><li>Rob Roy</li></a>  
+                    </div>
+                    <div className="about-body-content">
+                        <span>Technology Stack</span>
+                        <li>MongoDB</li>
+                        <li>Express</li>
+                        <li>React.js / Redux</li>
+                        <li>Node.js</li>
                     </div>
                 </div>
             </div>
@@ -32,3 +39,6 @@ class About extends React.Component {
 }
 
 export default About;
+
+// TODO: Consider adding technology icons in "Technology Stack" section
+// TODO: Consider adding team photos under "Team" section
