89dcc61054a220d926b01256d1b7cb573d659990
SkiesXR <pkrasnick@gmail.com>
2019-08-06 14:53:24 -0700

    About: Add GitHub section below header

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import about from '../../styles/about/about.scss';
+import GitHub from '../../assets/icons/GitHub.png';
 
 class About extends React.Component {
 
     render () {
 
         return(
-            <div>
-                <span>About Page</span>
+            <div className="about-container">
+                <div className="about-header">
+                    <h1>About</h1>
+                </div>
+                <div className="about-body">
+                    <div className="about-body-projectRepo">
+                        <span className="animate-flicker">GitHub Repo</span>
+                        <a href="https://github.com/BenjaminT88/shake_it_up" target="_blank" rel="noopener noreferrer">
+                            <img className={ `about-body-icon` } src={ GitHub } alt="GitHub"/>
+                        </a>
+                    </div>
+                </div>
             </div>
         ); 
     }
