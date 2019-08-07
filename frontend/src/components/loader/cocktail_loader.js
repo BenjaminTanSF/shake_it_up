e73d19875018599678fa0234abe2b64e769ed27b
SkiesXR <pkrasnick@gmail.com>
2019-08-06 17:39:33 -0700

    Loader: Add CSS styling

--- frontend/src/components/loader/cocktail_loader.js
+++ frontend/src/components/loader/cocktail_loader.js
@@ -5,11 +5,22 @@ class CocktailLoader extends React.Component {
     render() {
 
         return(
-            
-
-
-
-
+            <div className="loader-body">
+                <div id="loader">
+                    <div id="lemon"></div>
+                    <div id="straw"></div>
+                    <div id="glass">
+                        <div id="cubes">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div id="drink"></div>
+                        {/* <span id="counter"></span> */}
+                    </div>
+                    {/* <div id="coaster"></div> */}
+                </div>
+            </div>
         );
     }
 }
