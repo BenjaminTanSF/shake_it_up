b480c5b06f7ec0365aea4ef8ed9c409b0c18aaa9
SkiesXR <pkrasnick@gmail.com>
2019-08-06 19:47:48 -0700

    Loader: Convert jQuery code to React

--- frontend/src/components/loader/cocktail_loader.js
+++ frontend/src/components/loader/cocktail_loader.js
@@ -1,46 +1,100 @@
 import React from 'react';
+import loaderCSS from '../../styles/loader/loader.scss';
 
 class CocktailLoader extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            loaded: 0,
+            lemonOpacity: 0,
+            strawOpacity: 0,
+            glassOpacity: 0,
+            drinkOpacity: 0,
+            cubesOpacity: 0,
+            cube1Opacity: 0,
+            cube2Opacity: 0,
+            cube3Opacity: 0,
+            drinkTop: 100
+        }
+
+        // Binding methods 
         this.loading = this.loading.bind(this);
+
+        // Refs
+        this.lemon = React.createRef();
+        this.straw = React.createRef();
+        this.drink = React.createRef();
+        this.glass = React.createRef();
+        this.cubes = React.createRef();
+        this.cube1 = React.createRef();
+        this.cube2 = React.createRef();
+        this.cube3 = React.createRef();
+    }
+
+    componentDidMount() {
+        this.loading();
     }
 
     loading() {
+        // debugger;
         var worker = null;
-        var loaded = 0;
-        var lemon = document.getElementById('lemon');
-        var straw = document.getElementById('straw');
-        var cubes = document.getElementById('cubes');
-        var drink = document.getElementById('drink');
+        let that = this;
 
         function increment() {
-            // $('#counter').html(loaded + '%');
-            drink.style.top = `${100 - loaded * .9}%`;
-            if (loaded === 25) document.getElementById('cube1').style.opacity = 1
-            if (loaded === 50) document.getElementById('cube2').style.opacity = 1
-            if (loaded === 75) document.getElementById('cube3').style.opacity = 1
-            if (loaded == 100) {
-                lemon.style.opacity = 1;
-                straw.style.opacity = 1;
-                loaded = 0;
+            // debugger;
+            // this.drink.current.style.top = `${(100 - this.state.loaded * .9)}%`;
+            let loaded = that.state.loaded;
+            that.setState (() => {
+                // debugger;
+                return {drinkTop: (100 - loaded * .9)}
+            })
+            if (that.state.loaded === 25) {
+                that.setState({
+                    cube1Opacity: 1
+                })
+            }
+
+            if (that.state.loaded === 50) {
+                that.setState({
+                    cube2Opacity: 1
+                })
+            }
+
+            if (that.state.loaded === 75) {
+                that.setState({
+                    cube3Opacity: 1
+                })
+            }
+
+            if (that.state.loaded == 100) {
+                that.setState({
+                    lemonOpacity: 1,
+                    strawOpacity: 1,
+                    loaded: 0
+                })
                 stopLoading();
-                setTimeout(startLoading, 1000);
-            } else loaded++;
+            } else {
+                that.setState(prevState => {
+                    // debugger;
+                   return {loaded: prevState.loaded + 1}
+                });
+            }     
         }
 
         function startLoading() {
-            lemon.style.opacity = 0;
-            straw.style.opacity = 0;
-            cubes.style.opacity = 0;
-            worker = setInterval(increment, 30);
+            // that.setState({
+            //     lemonOpacity: 0,
+            //     strawOpacity: 0,
+            //     cubesOpacity: 0
+            // })
+            worker = setTimeout(increment, 30);
         }
 
         function stopLoading() {
             clearInterval(worker);
         }
-
+        
         startLoading();
     }
 
@@ -48,17 +102,18 @@ class CocktailLoader extends React.Component {
 
         return(
             <div className="loader-body">
+                {/* { this.loading() } */}
                 <div id="loader">
-                    <div id="lemon"></div>
-                    <div id="straw"></div>
-                    <div id="glass">
-                        <div id="cubes">
-                            <div className="cube" id="cube1"></div>
-                            <div className="cube" id="cube2"></div>
-                            <div className="cube" id="cube3"></div>
+                    <div id="lemon" ref={ this.lemon } style={{ opacity: this.state.lemonOpacity }}></div>
+                    <div id="straw" ref={ this.straw } style={{ opacity: this.state.strawOpacity }}></div>
+                    <div id="glass" ref={ this.glass } style={{ opacity: this.state.glassOpacity }}>
+                        <div id="cubes" ref={ this.cubes } style={{ opacity: this.state.cubesOpacity }}>
+                            <div className="cube" id="cube1" ref={ this.cube1 } style={{ opacity: this.state.cube1Opacity }}></div>
+                            <div className="cube" id="cube2" ref={ this.cube2 } style={{ opacity: this.state.cube2Opacity }}></div>
+                            <div className="cube" id="cube3" ref={ this.cube3 } style={{ opacity: this.state.cube3Opacity }}></div>
                         </div>
-                        <div id="drink"></div>
-                        <span id="counter"></span>
+                        <div id="drink" ref={ this.drink } style={{ opacity: this.state.drinkOpacity, top: `${this.state.drinkTop}%` }}></div>
+                        {/* <span id="counter"></span> */}
                     </div>
                     {/* <div id="coaster"></div> */}
                 </div>
