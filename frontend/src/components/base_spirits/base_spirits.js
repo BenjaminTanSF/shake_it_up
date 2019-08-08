f7458ccd68d11daca45ca8cddfe6d1cea651a375
SkiesXR <pkrasnick@gmail.com>
2019-08-07 21:21:49 -0700

    Discover-Desktop: Complete base spirit grid styling

--- frontend/src/components/base_spirits/base_spirits.js
+++ frontend/src/components/base_spirits/base_spirits.js
@@ -6,16 +6,12 @@ import '../../styles/discover/discover.scss';
 
 class BaseSpiritList extends React.Component {
 
-
   render() {
     return (
       <div className="discover-base">
         <h1 id="base-spirit-h1">Base Spirits</h1>
         <div className="discover-base-spiritlist">
-          {/* <Link id="d-bs-whiskey" className="discover-spirit" to="">Whiskey</Link> */}
-          <div className="bs-overlay">
-            <Link id="d-bs-whiskey" className="discover-spirit" to="ingredients/whiskey">Whiskey</Link>
-          </div>
+          <Link id="d-bs-whiskey" className="discover-spirit" to="ingredients/whiskey">Whiskey</Link>
           <Link id="d-bs-vodka" className="discover-spirit" to="ingredients/rum">Vodka</Link>
           <Link id="d-bs-tequila" className="discover-spirit" to="ingredients/vodka">Tequila</Link>
           <Link id="d-bs-rum" className="discover-spirit" to="ingredients/tequila">Rum</Link>
