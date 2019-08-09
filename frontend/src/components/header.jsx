22bbd6d73df136cf3c026d4f5fd86e428afd6f0e
BenjaminTan <yiren.tan@gmail.com>
2019-08-09 15:22:15 -0700

    Changed menu size and removed logger

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,18 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { withRouter, Link, NavLink } from 'react-router-dom';
+import discover from '../assets/icons/discover2x.png';
+import discoverActive from '../assets/icons/discover_red2x.png';
+import cocktail from '../assets/icons/cocktail.png';
+import cocktailActive from '../assets/icons/cocktail-red.png';
+import ingredient from '../assets/icons/ingredients.png';
+import ingredientActive from '../assets/icons/ingredients_red.png';
+import about from '../assets/icons/about.png';
+import aboutActive from '../assets/icons/about_red.png';
 
 class Header extends React.Component {
 
-    render() {
-        return (
-            <div className="discover-header">
-                <Link to="/">Shake it Up!</Link>
-            </div>
-        );
-    }
+	render() {
+		const { pathname } = this.props.history.location;
+
+		return (
+			<div className="discover-header">
+
+				<div className="menu-item-container mobile-hidden">
+					<NavLink exact to="/" className="menu-item-container" activeClassName="menu-active">
+						<img alt="menuItem" className="menu-icon" src={pathname === "/" ? discoverActive : discover}></img>
+						{/* <span>Discover</span> */}
+					</NavLink>
+				</div>
+
+				<div className="menu-item-container mobile-hidden">
+					<NavLink to="/drinks" className="menu-item-container" activeClassName="menu-active">
+						<img alt="menuItem" className="menu-icon" src={pathname.includes("drinks") ? cocktailActive : cocktail}></img>
+						{/* <span>Cocktails</span> */}
+					</NavLink>
+				</div>
+
+				<Link to="/">Shake it Up!</Link>
+
+				<div className="menu-item-container mobile-hidden">
+					<NavLink to="/ingredients" className="menu-item-container" activeClassName="menu-active">
+						<img alt="menuItem" className="menu-icon" src={pathname.includes("ingredients") ? ingredientActive : ingredient}></img>
+						{/* <span>Ingredients</span> */}
+					</NavLink>
+				</div>
+
+				<div className="menu-item-container mobile-hidden">
+					<NavLink to="/about" className="menu-item-container" activeClassName="menu-active">
+						<img alt="menuItem" className="menu-icon" src={pathname.includes("about") ? aboutActive : about}></img>
+						{/* <span>About</span> */}
+					</NavLink>
+				</div>
+
+			</div>
+		);
+	}
 
 }
 
-export default Header;
+export default withRouter(Header);
 
